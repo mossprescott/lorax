8677595f66ab1367ed1934a9a3934402e1c30685
Moss Prescott <moss@theprescotts.com>
2018-08-04 16:56:09 -0400

    Leiningen?

---
+++ proto/project.clj
@@ -0,0 +1,7 @@
+(defproject meta "0.0.0-SNAPSHOT"
+  :description "Leiningen project for Lorax"
+  :url "http://theprescotts.com/moss/lorax"
+  :license {:name "???"
+            :url "http://myass.com/license"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main meta.core)
